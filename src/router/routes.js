2cd2f6da77165eea20ff27680ca3cd7ac1d9d7b1
Rahul <rahulsonawanen@gmail.com>
2023-07-20 00:48:18 +0530

    changes in master

---
+++ src/router/routes.js
@@ -0,0 +1,10 @@
+//example routes file
+import Home from '@/views/Home';
+const routes = [{
+        path: '/',
+        name: 'Welcome',
+        component: Home
+    },
+
+];
+export default routes
